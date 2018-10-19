a13f43fc42cb57471a449951a3d7743f4cfa850e
tommy770221 <tommy770221@gmail.com>
2018-10-19 13:26:04 +0800

    修正 Singleton Thread  commit

--- src/thread/SingleTonDemo.java
+++ src/thread/SingleTonDemo.java
@@ -7,15 +7,30 @@ public class SingleTonDemo {
 
     public static void main(String[] args) {
 
+        ThreadDemo threadDemo1=new ThreadDemo();
+        ThreadDemo threadDemo2=new ThreadDemo();
+        threadDemo1.start();
+        threadDemo2.start();
+
     }
 }
 
+class ThreadDemo extends Thread{
+
+    public void run(){
+        Singleton singleton=Singleton.getInstance();
+    }
+}
 
 class Singleton{
 
-    private Singleton singleton=null;
+    public Singleton(){
+        System.out.println("創建 Singleton");
+    }
 
-    public Singleton getInstance(){
+    private static Singleton singleton=null;
+
+    public static Singleton getInstance(){
         if(singleton==null){
             synchronized (Singleton.class){
                 if(singleton==null){
